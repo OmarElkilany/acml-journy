73a81a4372bd2283e03939bc579aa6132ddc80b5
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-23 00:00:20 +0200

    feat: create components

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './authentication/register/register.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { TestComponent } from './authentication/test/test.component';
+
 const routes: Routes = [
   { path: 'register', pathMatch: 'full', component: RegisterComponent },
-  { path: 'login', pathMatch: 'full', component: LoginComponent }
+  { path: 'login', pathMatch: 'full', component: LoginComponent },
+  { path: 'test', pathMatch: 'full', component: TestComponent }
 ];
 
 @NgModule({
