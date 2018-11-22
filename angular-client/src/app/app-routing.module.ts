be0352d8ac3aa1840b49cc2274305dc91f51fd88
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-22 23:17:36 +0200

    feat: Create login and register components and route to them.

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { RegisterComponent } from './authentication/register/register.component';
+import { LoginComponent } from './authentication/login/login.component';
+const routes: Routes = [
+  { path: 'register', pathMatch: 'full', component: RegisterComponent },
+  { path: 'login', pathMatch: 'full', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
