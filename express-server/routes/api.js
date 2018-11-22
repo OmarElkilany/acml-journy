93883cd7381286b0b3c475bdda9c077a91c18b7f
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-22 20:42:24 +0200

    feat: create passport

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const router = express.Router();
 
 router.get('/', function(req, res, next) { res.send('Server is working, boy.'); });
+
+
 
 module.exports = router;
