fd2b402092e2f0606300c9d0cb9270c0f1f47528
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-23 12:41:14 +0200

    feat: add the register component

--- angular-client/src/app/auth/register/register.component.ts
+++ angular-client/src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { TokenPayload } from '../TokenPayload'
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,21 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  userCredentials: TokenPayload = {
+    email: '',
+    name: '',
+    password: ''
+  };
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  register() {
+    this.auth.register(this.userCredentials).subscribe(() => {
+      this.router.navigateByUrl('/auth/test');
+    }, (err) => {
+      console.error(err);
+    });
+  }
 
   ngOnInit() {
   }
