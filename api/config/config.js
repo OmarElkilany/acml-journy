e7a32fb5a26ef566de00d74c20f1bc4248b54657
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-26 22:33:11 +0200

    feat: Create docker-compose.yml

--- api/config/config.js
+++ api/config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	MONGO_URI: process.env.DATABASE_URI || 'mongodb://localhost:27017/journy'
+	MONGO_URI: 'mongodb://localhost:27017/journy'
+	// MONGO_URI: 'mongodb://database/journy'
 };
