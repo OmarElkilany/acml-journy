43e3bad9e88eb99865631d38a1fb13fc4356148a
OmarElkilany <omar.alkilani23@gmail.com>
2018-11-28 16:11:41 +0200

    Install ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,13 +25,21 @@ import {
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right',
+      maxOpened: 3
+    }),
     MatSidenavModule,
     MatIconModule,
     MatToolbarModule,
     MatListModule,
     MatButtonModule
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    ToastrService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
