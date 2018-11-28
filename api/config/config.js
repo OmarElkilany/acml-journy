3735edf9381930cd3e333a9fc3cfe34a6f351f4d
Omar Elsebai <omarelsebaig@gmail.com>
2018-11-28 05:51:18 +0200

    Set environment variables in server from config.env

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	MONGO_URI: 'mongodb://localhost:27017/journy'
+	MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/journy'
 	// MONGO_URI: 'mongodb://database/journy'
 };
